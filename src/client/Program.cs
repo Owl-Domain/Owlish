7a8909b12b2fd4cc093272cdd9c280478e621608
Nightowl286 <nightowl286@protonmail.com>
2026-03-13 22:17:21 +0000

    Add basic application host.

--- src/client/Program.cs
+++ src/client/Program.cs
@@ -1,11 +1,44 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 namespace OwlDomain.Owlish;
 
 public static class Program
 {
 	#region Functions
-	public static void Main()
+	public static async Task Main()
 	{
-		Console.WriteLine("Welcome to owlish.");
+		HostApplicationBuilderSettings settings = new()
+		{
+			ApplicationName = "owlish",
+			DisableDefaults = true,
+			ContentRootPath = AppContext.BaseDirectory,
+#if DEBUG
+			EnvironmentName = Environments.Development,
+#endif
+		};
+
+		HostApplicationBuilder builder = Host.CreateEmptyApplicationBuilder(settings);
+		builder.Services.AddHostedService<OwlishWorker>();
+
+		IHost host = builder.Build();
+		try
+		{
+			await host.StartAsync();
+		}
+		finally
+		{
+			await host.StopAsync();
+		}
 	}
 	#endregion
+}
+
+class OwlishWorker : BackgroundService
+{
+	protected override Task ExecuteAsync(CancellationToken stoppingToken)
+	{
+		Console.WriteLine("Welcome to owlish.");
+		return Task.CompletedTask;
+	}
 }
